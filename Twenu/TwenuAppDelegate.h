426f64d1ba57e78e6b0e51a39ce7ebaf8b84d3f7
Kyle Kirby <kylekirby@optimalconnection.net>
2012-05-04 19:28:41 -0400

    Initial Commit

---
+++ Twenu/TwenuAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TwenuAppDelegate.h
+//  Twenu
+//
+//  Created by Kyle Kirby on 5/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TwenuAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Twenu/TwenuAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  TwenuAppDelegate.m
+//  Twenu
+//
+//  Created by Kyle Kirby on 5/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TwenuAppDelegate.h"
+
+@implementation TwenuAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
